698afc03c6460081968b440f7e27d188ea352da6
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-04-25 17:10:01 +0200

    Add exception handling

--- src/main/java/app/commands/HistoryCommand.java
+++ src/main/java/app/commands/HistoryCommand.java
@@ -24,7 +24,13 @@ public class HistoryCommand extends CommandListener {
         int topN = Integer.parseInt(parameters[0]);
         int limit = Integer.parseInt(parameters[1]);
         TextChannel channel = event.getTextChannel();
-        List<Message> messages = channel.getHistoryFromBeginning(limit).complete().getRetrievedHistory();
+        List<Message> messages;
+        try {
+            messages = channel.getHistoryFromBeginning(limit).complete().getRetrievedHistory();
+        } catch (IllegalArgumentException ex) {
+            channel.sendMessage("Limit may not exceed 100!").queue();
+            return;
+        }
         StringBuilder content = new StringBuilder();
         for (Message message : messages) {
             content.append(message.getContentRaw()).append(" ");
