bb99d1129d262714c140b076a9ffc5732b9fef25
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-04-25 21:17:07 +0200

    Add capitalize function

--- src/main/java/app/utils/StringUtil.java
+++ src/main/java/app/utils/StringUtil.java
@@ -62,4 +62,8 @@ public class StringUtil {
 
         return topTen.toString();
     }
+
+    public static String capitalize(String text) {
+        return text.substring(0, 1).toUpperCase() + text.substring(1);
+    }
 }
